a30f2dbea99fd92b90a71f9c5cc5cf91d3a1a278
Saurabh Singh <saurabhsinghpit@gmail.com>
2026-09-14 23:51:01 +0530

    practiced array and manupulation along with searching algorithm

---
+++ day04_practice/OnlyEven.java
@@ -0,0 +1,26 @@
+package day04_practice;
+
+import java.util.Scanner;
+
+public class OnlyEven {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the length of an array ");
+        int l = sc.nextInt();
+        int[] a = new int[l];
+        int num;
+
+        System.out.println("Enter the elements: ");
+
+        for(int i=0;i<l;i++){
+            num=sc.nextInt();
+            if(num%2==0){
+                a[i]=num;
+            }else{
+                System.out.println("Enter even elements!!");
+                i--;
+            }
+            
+        }
+    }
+}
